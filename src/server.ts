20c3574f13752995a6768d114a5e64da7ffbd321
GuyBR <gbrobbe@gmail.com>
2020-07-10 17:28:08 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
@@ -12,9 +12,9 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
-app.use(cors());
 app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
