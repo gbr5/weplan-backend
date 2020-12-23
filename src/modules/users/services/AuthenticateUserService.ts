7fad46f94a38155b56eae3ec494e7ce44161e786
GuyBR <gbrobbe@gmail.com>
2020-12-23 20:41:28 -0300

    S_73-CreateDefault UserFileCategories

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -7,6 +7,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IHashProvider from '@modules/users/providers/hashProviders/models/IHashProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import IUserFileCategoriesRepository from '../repositories/IUserFileCategoriesRepository';
 
 interface IRequest {
   email: string;
@@ -23,6 +24,9 @@ class AuthenticateUserService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
+    @inject('UserFileCategoriesRepository')
+    private userFileCategoriesRepository: IUserFileCategoriesRepository,
+
     @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
@@ -32,6 +36,119 @@ class AuthenticateUserService {
 
     if (!user) {
       throw new AppError('Invalid e-mail adress/password combination.', 401);
+    }
+
+    const userFileCategories = await this.userFileCategoriesRepository.findByUserId(
+      user.id,
+    );
+
+    if (!userFileCategories) {
+      Promise.all([
+        this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Transações',
+          description: 'Comprovantes de transações.',
+          color: 'rgba(229, 93, 93, 1)',
+        }),
+        this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Contratos',
+          description: 'Contratos de produtos e serviços.',
+          color: 'rgba(97, 181, 255, 1)',
+        }),
+        this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Orçamentos',
+          description: 'Orçamentos de produtos e serviços.',
+          color: 'rgba(233, 208, 119, 1)',
+        }),
+        this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Convites',
+          description: 'Convites dos seus eventos.',
+          color: 'rgba(151, 233, 119, 1)',
+        }),
+        this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Convites de Amigos',
+          description: 'Convites dos eventos de amigos.',
+          color: 'rgba(233, 172, 119, 1)',
+        }),
+        this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Atas de Reuniões',
+          description: 'Atas de reuniões com fornecedores e/ou parceiros.',
+          color: 'rgba(176, 242, 232, 1)',
+        }),
+      ]);
+    } else {
+      const transactionsCategory = userFileCategories.find(
+        category => category.name === 'Transações',
+      );
+      const contractsCategory = userFileCategories.find(
+        category => category.name === 'Contratos',
+      );
+      const budgetsCategory = userFileCategories.find(
+        category => category.name === 'Orçamentos',
+      );
+      const invitationsCategory = userFileCategories.find(
+        category => category.name === 'Convites',
+      );
+      const friendsInvitationsCategory = userFileCategories.find(
+        category => category.name === 'Convites de Amigos',
+      );
+      const minutesOfMeetingsCategory = userFileCategories.find(
+        category => category.name === 'Atas de Reuniões',
+      );
+
+      if (!transactionsCategory) {
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Transações',
+          description: 'Comprovantes de transações.',
+          color: 'rgba(229, 93, 93, 1)',
+        });
+      }
+      if (!contractsCategory) {
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Contratos',
+          description: 'Contratos de produtos e serviços.',
+          color: 'rgba(97, 181, 255, 1)',
+        });
+      }
+      if (!budgetsCategory) {
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Orçamentos',
+          description: 'Orçamentos de produtos e serviços.',
+          color: 'rgba(233, 208, 119, 1)',
+        });
+      }
+      if (!invitationsCategory) {
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Convites',
+          description: 'Convites dos seus eventos.',
+          color: 'rgba(151, 233, 119, 1)',
+        });
+      }
+      if (!friendsInvitationsCategory) {
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Convites de Amigos',
+          description: 'Convites dos eventos de amigos.',
+          color: 'rgba(233, 172, 119, 1)',
+        });
+      }
+      if (!minutesOfMeetingsCategory) {
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Atas de Reuniões',
+          description: 'Atas de reuniões com fornecedores e/ou parceiros.',
+          color: 'rgba(176, 242, 232, 1)',
+        });
+      }
     }
 
     const passwordMatched = await this.hashProvider.compareHash(
