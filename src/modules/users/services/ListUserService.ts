2b460471f426f179dd36ce152925d9c0a4258e04
GuyBR <gbrobbe@gmail.com>
2021-01-23 02:02:08 -0300

    Fix:booleanQuestionWindow & UserProfileM /SideMenu

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -1,6 +1,5 @@
 import { injectable, inject } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -15,13 +14,7 @@ class ListUserService {
   public async execute(): Promise<User[]> {
     const users = await this.usersRepository.findAllUsers();
 
-    if (!users) {
-      throw new AppError('Company information not found.');
-    }
-
-    const sortedUsers = users.filter(user => user.personInfo);
-
-    return sortedUsers;
+    return users;
   }
 }
 
