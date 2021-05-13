0757370a8c7073b89c1262c888cfec16ae3a888b
GuyBR <gbrobbe@gmail.com>
2021-05-12 23:00:23 -0300

    S_293:Server-Simplified User Auth

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -38,136 +38,6 @@ class AuthenticateUserService {
       throw new AppError('Invalid e-mail adress/password combination.', 401);
     }
 
-    const userFileCategories = await this.userFileCategoriesRepository.findByUserId(
-      user.id,
-    );
-
-    if (!userFileCategories) {
-      Promise.all([
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Transações',
-          description: 'Comprovantes de transações.',
-          color: 'rgba(229, 93, 93, 1)',
-        }),
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Contratos',
-          description: 'Contratos de produtos e serviços.',
-          color: 'rgba(97, 181, 255, 1)',
-        }),
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Orçamentos',
-          description: 'Orçamentos de produtos e serviços.',
-          color: 'rgba(233, 208, 119, 1)',
-        }),
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Convites',
-          description: 'Convites dos seus eventos.',
-          color: 'rgba(151, 233, 119, 1)',
-        }),
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Convites de Amigos',
-          description: 'Convites dos eventos de amigos.',
-          color: 'rgba(233, 172, 119, 1)',
-        }),
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Atas de Reuniões',
-          description: 'Atas de reuniões com fornecedores e/ou parceiros.',
-          color: 'rgba(176, 242, 232, 1)',
-        }),
-        this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Outros',
-          description: 'Demais arquivos.',
-          color: 'rgba(250, 252, 232, 1)',
-        }),
-      ]);
-    } else {
-      const transactionsCategory = userFileCategories.find(
-        category => category.name === 'Transações',
-      );
-      const contractsCategory = userFileCategories.find(
-        category => category.name === 'Contratos',
-      );
-      const budgetsCategory = userFileCategories.find(
-        category => category.name === 'Orçamentos',
-      );
-      const invitationsCategory = userFileCategories.find(
-        category => category.name === 'Convites',
-      );
-      const friendsInvitationsCategory = userFileCategories.find(
-        category => category.name === 'Convites de Amigos',
-      );
-      const minutesOfMeetingsCategory = userFileCategories.find(
-        category => category.name === 'Atas de Reuniões',
-      );
-      const othersCategory = userFileCategories.find(
-        category => category.name === 'Outros',
-      );
-
-      if (!transactionsCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Transações',
-          description: 'Comprovantes de transações.',
-          color: 'rgba(229, 93, 93, 1)',
-        });
-      }
-      if (!contractsCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Contratos',
-          description: 'Contratos de produtos e serviços.',
-          color: 'rgba(97, 181, 255, 1)',
-        });
-      }
-      if (!budgetsCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Orçamentos',
-          description: 'Orçamentos de produtos e serviços.',
-          color: 'rgba(233, 208, 119, 1)',
-        });
-      }
-      if (!invitationsCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Convites',
-          description: 'Convites dos seus eventos.',
-          color: 'rgba(151, 233, 119, 1)',
-        });
-      }
-      if (!friendsInvitationsCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Convites de Amigos',
-          description: 'Convites dos eventos de amigos.',
-          color: 'rgba(233, 172, 119, 1)',
-        });
-      }
-      if (!minutesOfMeetingsCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Atas de Reuniões',
-          description: 'Atas de reuniões com fornecedores e/ou parceiros.',
-          color: 'rgba(176, 242, 232, 1)',
-        });
-      }
-      if (!othersCategory) {
-        await this.userFileCategoriesRepository.create({
-          user_id: user.id,
-          name: 'Outros',
-          description: 'Demais arquivos.',
-          color: 'rgba(250, 252, 232, 1)',
-        });
-      }
-    }
-
     const passwordMatched = await this.hashProvider.compareHash(
       password,
       user.password,
@@ -175,6 +45,136 @@ class AuthenticateUserService {
 
     if (!passwordMatched) {
       throw new AppError('Invalid e-mail adress/password combination.', 401);
+    }
+
+    const userFileCategories = await this.userFileCategoriesRepository.findByUserId(
+      user.id,
+    );
+
+    if (!userFileCategories) {
+      Promise.all([
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Transações',
+          description: 'Comprovantes de transações.',
+          color: 'rgba(229, 93, 93, 1)',
+        }),
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Contratos',
+          description: 'Contratos de produtos e serviços.',
+          color: 'rgba(97, 181, 255, 1)',
+        }),
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Orçamentos',
+          description: 'Orçamentos de produtos e serviços.',
+          color: 'rgba(233, 208, 119, 1)',
+        }),
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Convites',
+          description: 'Convites dos seus eventos.',
+          color: 'rgba(151, 233, 119, 1)',
+        }),
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Convites de Amigos',
+          description: 'Convites dos eventos de amigos.',
+          color: 'rgba(233, 172, 119, 1)',
+        }),
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Atas de Reuniões',
+          description: 'Atas de reuniões com fornecedores e/ou parceiros.',
+          color: 'rgba(176, 242, 232, 1)',
+        }),
+        await this.userFileCategoriesRepository.create({
+          user_id: user.id,
+          name: 'Outros',
+          description: 'Demais arquivos.',
+          color: 'rgba(250, 252, 232, 1)',
+        }),
+      ]);
+      // } else {
+      //   const transactionsCategory = userFileCategories.find(
+      //     category => category.name === 'Transações',
+      //   );
+      //   const contractsCategory = userFileCategories.find(
+      //     category => category.name === 'Contratos',
+      //   );
+      //   const budgetsCategory = userFileCategories.find(
+      //     category => category.name === 'Orçamentos',
+      //   );
+      //   const invitationsCategory = userFileCategories.find(
+      //     category => category.name === 'Convites',
+      //   );
+      //   const friendsInvitationsCategory = userFileCategories.find(
+      //     category => category.name === 'Convites de Amigos',
+      //   );
+      //   const minutesOfMeetingsCategory = userFileCategories.find(
+      //     category => category.name === 'Atas de Reuniões',
+      //   );
+      //   const othersCategory = userFileCategories.find(
+      //     category => category.name === 'Outros',
+      //   );
+
+      //   if (!transactionsCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Transações',
+      //       description: 'Comprovantes de transações.',
+      //       color: 'rgba(229, 93, 93, 1)',
+      //     });
+      //   }
+      //   if (!contractsCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Contratos',
+      //       description: 'Contratos de produtos e serviços.',
+      //       color: 'rgba(97, 181, 255, 1)',
+      //     });
+      //   }
+      //   if (!budgetsCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Orçamentos',
+      //       description: 'Orçamentos de produtos e serviços.',
+      //       color: 'rgba(233, 208, 119, 1)',
+      //     });
+      //   }
+      //   if (!invitationsCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Convites',
+      //       description: 'Convites dos seus eventos.',
+      //       color: 'rgba(151, 233, 119, 1)',
+      //     });
+      //   }
+      //   if (!friendsInvitationsCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Convites de Amigos',
+      //       description: 'Convites dos eventos de amigos.',
+      //       color: 'rgba(233, 172, 119, 1)',
+      //     });
+      //   }
+      //   if (!minutesOfMeetingsCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Atas de Reuniões',
+      //       description: 'Atas de reuniões com fornecedores e/ou parceiros.',
+      //       color: 'rgba(176, 242, 232, 1)',
+      //     });
+      //   }
+      //   if (!othersCategory) {
+      //     await this.userFileCategoriesRepository.create({
+      //       user_id: user.id,
+      //       name: 'Outros',
+      //       description: 'Demais arquivos.',
+      //       color: 'rgba(250, 252, 232, 1)',
+      //     });
+      //   }
     }
 
     const { secret, expiresIn } = authConfig.jwt;
